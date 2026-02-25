75904b8820f97d6011b9fe0261bb953c49dcfba9
Peter Good <peterwgood77@gmail.com>
2026-02-25 14:47:15 -0500

    Add GitHub Pages deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Required for GitHub Pages static export
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't have a built-in image server
+  },
+  // If your site is at Peterbgood.github.io/my-portfolio/, 
+  // you MUST uncomment the line below and set it to your repo name:
+  // basePath: '/my-portfolio', 
 };
 
 export default nextConfig;
